b87453dc82de1db54251c051671c597b9e6ef6cc
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-08-08 02:36:33 +0530

    Just Me Project

--- mvc/controllers/index.js
+++ mvc/controllers/index.js
@@ -2,11 +2,25 @@
 let data = require("../../data");
 const postData = data.postData;
 const uniqueTags = data.uniqueTags;
-const categoryData = data.categoryData;
 
+const defaultData = {
+    categoryData : data.categoryData
+};
 const recentPostsAmount = 5;
+const rightSideBar = {
+    uniqueTags:uniqueTags,
+    recentPosts: postData.slice(0,recentPostsAmount)
+};
+
 const getHomePage = function(req,res) {
-    res.render("index",{title : "Just Me",posts:postData,active:"index",categoryData:categoryData});
+
+    let data = {
+        ...defaultData,
+        title:"Just Me",
+        posts:postData,
+        active:"index"
+    }
+    res.render("index",data);
 }
 
 const getBlogPost = function({params},res) {
@@ -14,28 +28,55 @@ const getBlogPost = function({params},res) {
     if (!post) {
         res.redirect('/404');
     }
-    res.render("post",{title: post.title,post:post,uniqueTags:uniqueTags,recentPosts: postData.slice(0,recentPostsAmount),categoryData:categoryData});
+    let data = {
+        ...defaultData,
+        title:post.title,
+        post:post,
+        active:"index",
+        ...rightSideBar
+    }
+    res.render("post",data);
 }
 const get404 = function(req,res) {
-    res.render('404',{title : "404-I Couldn't find that Page.",uniqueTags:uniqueTags,recentPosts: postData.slice(0,recentPostsAmount),categoryData:categoryData});
+    let data = {
+        ...defaultData,
+        title:"404-I Couldn't find that Page.",
+        ...rightSideBar
+    }
+    res.render('404',data);
 }
 const redirect404 = function(req,res) {
     res.redirect('/404');
 }
 
 const getAbout = function(req,res) {
-    res.render('About',{title : "About Me",active:"about",categoryData:categoryData});
+    let data = {
+        ...defaultData,
+        title:"About Me",
+        active:"about"
+    }
+    res.render('About',data);
 }
 const getContact = function(req,res) {
-    res.render('Contact',{title : "Contact Me",active:"contact",categoryData:categoryData});
+    let data = {
+        ...defaultData,
+        title:"Contact Me",
+        active:"contact"
+    }
+    res.render('Contact',data);
 }
 const getFilterList = function({query},res) {
     let filterPosts = postData.filter((val) =>
-      val.category=== query.category || val.tags.includes(query.tag);
+      val.category=== query.category || val.tags.includes(query.tag)
     );
+    let data = {
+        ...defaultData,
+        title:"Just Me - Filtered",
+        active:query.category,
+        posts:filterPosts
+    }
 
-    res.render('Filter',{title : "Just Me",
-    active:query.category,posts:filterPosts,categoryData:categoryData});
+    res.render('Filter',data);
 }
 module.exports = {
     getHomePage,
