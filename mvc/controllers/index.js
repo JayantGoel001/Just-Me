7639ea9d87b94a40b728246df00d76ac598e514e
Jayant Goel <jgoel92@gmail.com>
2021-06-30 11:07:47 +0530

    Initial Commit

--- mvc/controllers/index.js
+++ mvc/controllers/index.js
@@ -1,5 +1,92 @@
+let postData = [
+    {
+        title: "The Problem With The New Fashion",
+        body: "Measurement tailor cut commercial catwalk quality. Conformity stock beautiful creative sewing production innovation combination brand trade. Vintage jeans innovation petticoat attractive vogue showcase buttons apron. Bargain identity collection edge jersey independant outfit influence easy stylish luxurious. Modern elegant catwalk creative apron. Popular handbag glossy impeccable replicate celebrities purse bold industry original manufacture. Trendwatching pret-a-porter prediction buttons pumps valuable tailored easy petticoat enhance quantity clothes imagination hand-made. Purchase consumer identity wardrobe swag tones old-fashioned pret-a-porter value glitter. Item cheap urban clothing elegant. Stock petticoat illustration radical.",
+        category: "fashion",
+        image: "image_1.jpg",
+        comments: 3,
+        date: "Sept, 13, 2019"
+    }, {
+        title: "So You've Bought Life ... Now What?",
+        body: "Ensemble vintage piece leotard tailor swag purchase textile quality extraordinary swim-wear posture. Quality jumper urban tones mainstream bold effect illustration shawl purchase jeans. Fashion cut imagination wholesale wardrobe creative shade classic bold brand imprint. Creative identity modification couture innovation tailor etiquette elegant xs inexpensive glossy stitching. Illustration sleeveless expensive attractive independant limited runway cheap jersey manufacture artificial synthetic measurement enhance. Classic jewelry imprint clothing manufacture catwalk effect original. Etiquette consumer jewelry shape bargain creative pattern illustration minimalist replicate adjustment lingerie. Bodice affection Haute-couture. Valuable tones expirement tailor. Vintage jeans one-of-a-kind piece proportion ready-made jersey creative inexpensive popular phenomenon artistry purchase.",
+        category: "life",
+        image: "image_2.jpg",
+        comments: 12,
+        date: "Sept, 19, 2019"
+    }, {
+        title: "15 Secretly Funny People",
+        body: "Proportion color production attractive embroidery enhance beautiful ribbon fashion tailor instagram glossy retailer. Clothes price braiding breathable apparel glossy bodice. Zipper pastel garment consumer braiding. Waistline instagram effect mainstream vintage edge trade prediction quality wholesale handbag innovation pret-a-porter. Conservative expirement shape commercial breathable identity motif condition couture. Adjustment sportswear jersey condition luxurious. Ensemble hand-made vintage attractive classic effect craftmanship celebrities revealing shape. Hippie imprint jewelry creative prediction look quality contemporary trendwatching jacket collection. Proportion quality look accessory pattern apron luxurious pumps textile. Innovation hair independant artificial zipper clothes outlet radical showcase effect necessity hanger.",
+        category: "fun",
+        image: "image_3.jpg",
+        comments: 7,
+        date: "Sept, 21, 2019"
+    }, {
+        title: "5 Killer Quora Answers",
+        body: "Revealing lingerie comfortable tailor jewelry impeccable cut apparel. Conservative classic imprint expirement independant quantity. Mannequin independant elegant pastel breathable catwalk mainstream contemporary. Elegant collection xs wholesale phenomenon hair clothes identity shape catwalk sewing vintage. Handbag original zipper tailor emphasis textile identity trendwatching apron combination retailer proportion stitching. Original signature replicate. Artistry craftmanship glossy synthetic comfortable shade. Jeans bodice expensive jacket artistry buttons creative manufacture young quantity attractive taste trade. Classic influence identity innovation brand sari expirement price garment. Modification jeans limited jacket photography consumer pattern sportswear innovation imprint etiquette.",
+        category: "fun",
+        image: "image_4.jpg",
+        comments: 5,
+        date: "Sept, 29, 2019"
+    }, {
+        title: "The Death Of Fashion",
+        body: "Wholesale ready-made innovation commercial fashion prediction jersey posture. Jacket pumps item proportion jewelry high heels instagram hippie clothes illustration. Tones garment minimalist swim-wear clothing hair enhance model wardrobe. Illustration ensemble runway vogue quantity creative original casual pret-a-porter production leotard extraordinary trade wardrobe. Production modern independant outfit catwalk textile waistline shape halter high heels innovation modification. Tailored shade taste wholesale influence conformity jersey identity. Jewelry identity artistry. Embroidery bold expirement buttons label jewelry. Radical tailor wardrobe ensemble item enhance artificial conservative glitter artistry retailer elegant comfortable condition. Ready-made make up imagination tones shade affection production imprint artistry condition proportion bold clothes wardrobe.",
+        category: "fashion",
+        image: "image_5.jpg",
+        comments: 6,
+        date: "Oct, 10, 2019"
+    }, {
+        title: "What Will Cool Be Like in 100 Years?",
+        body: "Limited swim-wear modern. Clothes hand-made braiding extraordinary influence ribbon quality purse xl conservative. Clothing high heels beautiful fashion one-of-a-kind combination shawl. Cheap adjustment minimalist one-of-a-kind clothes clothing trade trendwatching inspiration ensemble ready-made. Enhance couture innovation unique trademark. Bargain necessity runway jumper beautiful trend imprint allure availability. Pumps modern jacket hippie expensive casual bodice. Artificial modification inspiration proportion bodice xl consumer retailer revealing. Quantity availability pumps condition attractive synthetic classic. Imprint illustration tailor mainstream industry.",
+        category: "fun",
+        image: "image_6.jpg",
+        comments: 10,
+        date: "Oct, 15, 2019"
+    }, {
+        title: "Fun: 10 Things I Wish I'd Known Earlier",
+        body: "Zipper vintage measurement necessity sari piece xl fashion. Swag pret-a-porter prediction artistry availability breathable original swim-wear. Ribbon clothes trend xl jumper wholesale. Condition attractive creative comfortable purse apron consumer shape hand-made trademark petticoat. Stock ensemble cheap jersey young hanger. Combination apron condition modern pastel embroidery innovation ready-made. Taste breathable adjustment tailored. Beautiful manufacture elegant. Shape bodice high heels artistic impeccable expensive. Piece sleeveless creative fashion casual color urban limited impeccable pret-a-porter garment necessity.",
+        category: "fun",
+        image: "image_7.jpg",
+        comments: 13,
+        date: "Oct, 23, 2019"
+    }, {
+        title: "12 Helpful Tips For Doing Life",
+        body: "Etiquette popular value halter hair influence classic emphasis phenomenon modern. Revealing item emphasis celebrities waistline. Jewelry look sewing imagination conformity. Runway creative instagram xs conservative hair luxurious manufacture identity. High heels prediction buttons pastel retailer sari price quality catwalk showcase artistic trend. Independant industry jacket young consumer pastel item bodice illustration lingerie minimalist pumps. Inexpensive modification apron artistic outlet production sportswear. Apparel affection trend celebrities purse. Stylish imprint easy popular conformity. Urban vogue tailored.",
+        category: "life",
+        image: "image_8.jpg",
+        comments: 2,
+        date: "Oct, 27, 2019"
+    }, {
+        title: "17 Reasons Why You Should Ignore People",
+        body: "Make up handbag availability price inspiration revealing value conformity shade trade industry innovation. High heels production shape sari trend taste xl mannequin conformity modern easy expirement garment. Glitter innovation conformity. Celebrities artificial unique collection imagination high heels clothes young color. Bodice revealing classic pret-a-porter petticoat influence pastel consumer valuable. Allure hippie posture couture ready-made inspiration bargain tailor jacket outlet glossy leotard quality inexpensive. Impeccable attractive elegant measurement proportion jewelry collection. Quality item celebrities high heels measurement imprint garment innovation accessory purchase consumer combination. Classic luxurious contemporary embroidery make up inspiration modern item. Swag stock runway.",
+        category: "life",
+        image: "image_9.jpg",
+        comments: 8,
+        date: "Oct, 30, 2019"
+    }, {
+        title: "The Ultimate Cheat Sheet for Travel",
+        body: "Affection posture lingerie signature apparel label. Handbag availability purchase. Jeans posture artistic wardrobe purse influence consumer limited sari bold. Quality leotard trend independant xs. Catwalk prediction showcase production. Minimalist lingerie combination quantity zipper proportion wardrobe effect imprint purse classic. Stitching artistic price garment outfit cheap zipper vogue stock showcase. Jewelry sportswear shawl sari mannequin. High heels label leotard attractive ensemble. Value photography jersey hair minimalist edge sleeveless apron bodice measurement item quantity modern pastel.",
+        category: "travel",
+        image: "image_10.jpg",
+        comments: 8,
+        date: "Nov, 4, 2019"
+    }, {
+        title: "5 Things Everyone Gets Wrong About Love",
+        body: "Expensive stylish color synthetic extraordinary shawl clothing combination easy wardrobe piece pastel. Make up xl contemporary brand fashion cut. Brand ensemble garment swag. Easy measurement hanger apparel imagination attractive emphasis. Pattern hanger jewelry brand purse. Trendwatching quantity collection. Hanger sleeveless label. Skirt sewing conservative attractive expensive photography ribbon limited value handbag contemporary elegant popular mannequin. Modern handbag luxurious valuable conformity bows vogue extraordinary elegant. Zipper mode ensemble.",
+        category: "love",
+        image: "image_11.jpg",
+        comments: 12,
+        date: "Nov, 9, 2019"
+    }, {
+        title: "How to Sell Love to a Skeptic",
+        body: "Clothing imprint price prediction minimalist fashion edge bold contemporary embroidery halter emphasis garment. One-of-a-kind bodice glitter retailer valuable industry photography independant availability accessory bows zipper ribbon trendwatching. One-of-a-kind consumer prediction label industry influence hanger look easy jumper comfortable photography halter mainstream. Clothes shape expensive popular purse artistic. Conformity craftmanship modern artificial combination comfortable. Etiquette embroidery inexpensive hanger sportswear leotard look swim-wear shawl production manufacture pret-a-porter. Inspiration stock zipper casual consumer cheap extraordinary item piece vogue catwalk craftmanship. Garment high heels beautiful modern leotard inspiration condition imagination taste cut. Elegant petticoat luxurious photography easy effect sportswear couture stock. Outlet pumps skirt conservative combination glitter proportion jersey.",
+        category: "love",
+        image: "image_12.jpg",
+        comments: 18,
+        date: "Nov, 16, 2019"
+    }
+];
 const getHomePage = (req,res)=>{
-    res.render('index',{ title : "Just Me" });
+    res.render('index',{ title : "Just Me", posts : postData });
 }
 
 module.exports = {
