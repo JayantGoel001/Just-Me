2919a2434bd870a38c53f7cbf10e5dd8d3bc89ae
Jayant Goel <jgoel92@gmail.com>
2021-07-01 03:53:36 +0530

    Initial Commit

--- mvc/controllers/index.js
+++ mvc/controllers/index.js
@@ -5,9 +5,21 @@ const uniqueTags = data.uniqueTag;
 const uniqueCategories = data.uniqueCategory;
 const archives = data.uniqueDate;
 const numberOfRecentPost = 3;
-
+const defaultData = {
+    categories : uniqueCategories
+}
+const sidebarData = {
+    tags:uniqueTags,
+    archives : archives
+}
 const getHomePage = (req,res)=>{
-    res.render('index',{ title : "Just Me", posts : postData ,active : "index", categories : uniqueCategories});
+    let data = {
+        ...defaultData,
+        title : "Just Me",
+        posts : postData ,
+        active : "index"
+    }
+    res.render('index',data);
 }
 const getBlogPost = (req,res)=>{
     let post = postData.find( val => val.id.toString() === req.params.postID);
@@ -15,28 +27,57 @@ const getBlogPost = (req,res)=>{
         res.redirect('/404');
     }
     let randomNumber = Math.floor(Math.random() * (postData.length - numberOfRecentPost));
-    res.render('post', { title : post.title , post : post, tags:uniqueTags, categories : uniqueCategories, recentPost : postData.slice(randomNumber,randomNumber+numberOfRecentPost), archives : archives });
+    let data = {
+        ...defaultData,
+        ...sidebarData,
+        title : post.title ,
+        recentPost : postData.slice(randomNumber,randomNumber+numberOfRecentPost),
+        post : post
+    }
+    res.render('post', data);
 }
 
 const getFilteredPost = (req,res) =>{
     let posts = postData.filter( val=> {
         return val.category === req.query.category || val.tags.includes(req.query.tag);
     });
-    res.render('filter',{ title:"Just Me - "+req.query.category || req.query.tag, posts: posts, categories : uniqueCategories,active : req.query.category });
+    let data = {
+        ...defaultData,
+        title:"Just Me - "+req.query.category || req.query.tag,
+        posts: posts,
+        active : req.query.category
+    }
+    res.render('filter', data);
 }
 
 const get404 = (req,res)=>{
     let randomNumber = Math.floor(Math.random() * (postData.length - numberOfRecentPost));
-    res.render('404', { title : "4O4 - I couldn't find that page...", tags:uniqueTags,categories : uniqueCategories, recentPost : postData.slice(randomNumber,randomNumber+numberOfRecentPost),archives : archives });
+    let data = {
+        ...defaultData,
+        ...sidebarData,
+        recentPost : postData.slice(randomNumber,randomNumber+numberOfRecentPost),
+        title : "4O4 - I couldn't find that page..."
+    }
+    res.render('404', data);
 }
 const redirect404 = (req,res)=>{
     res.redirect('/404');
 }
 const getAbout = (req,res)=>{
-    res.render('about', { title : 'About Me'  ,active : "about"});
+    let data = {
+        ...defaultData,
+        title : 'About Me',
+        active : "about"
+    }
+    res.render('about', data);
 }
 const getContact = (req,res)=>{
-    res.render('contact', { title : 'Contact Me'  ,active : "contact"});
+    let data = {
+        ...defaultData,
+        title : 'Contact Me',
+        active : "contact"
+    }
+    res.render('contact', data);
 }
 module.exports = {
     getHomePage,
