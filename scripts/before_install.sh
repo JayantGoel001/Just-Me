bbda8c88b18127dfd2c91b22f981d28306f72d95
Jayant Goel <jgoel92@gmail.com>
2022-05-28 03:30:20 +0530

    Initial Commit
    Signed-off-by: Jayant Goel <jgoel92@gmail.com>

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,7 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install 16
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/just-me"
