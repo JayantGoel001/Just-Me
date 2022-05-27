f3da5083423a61cc2c08427e19cae84a287ebc0f
JayantGoel001 <jgoel92@gmail.com>
2022-05-28 02:40:55 +0530

    Initial Commit
    Signed-off-by: Jayant Goel <jgoel92@gmail.com>

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+#give permission for everything in the just-me directory
+sudo chmod -R 777 /home/ec2-user/just-me
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /home/ec2-user/just-me
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
